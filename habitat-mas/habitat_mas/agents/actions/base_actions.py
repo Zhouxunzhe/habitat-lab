4ed3f296ef3180ebffcbd3747db70821eddd6d7c
SgtVincent <sgt79911030@gmail.com>
2024-05-13 21:28:40 +0800

    add minimal actions for crab agents

--- habitat-mas/habitat_mas/agents/actions/base_actions.py
+++ habitat-mas/habitat_mas/agents/actions/base_actions.py
@@ -2,32 +2,54 @@ import os
 from typing import List, Dict, Tuple, Union
 import numpy as np
 from habitat import Env
-from agents.actions.action_server import ActionServer
+
 from habitat.tasks.rearrange.actions.oracle_nav_action import (
     OracleNavCoordinateAction,
     OracleNavAction
 )
 
+from crab import action
 
-class BaseActions:
+# TODO: to connect with the habitat-lab/ habitat-mas agents. 
+
+@action
+def get_agents() -> List[str]:
     """
-    This class is used to collect all interface action APIs between LLM agents and the habitat simulator. 
+    Get the list of agents in the environment.
+    
+    Args:
+        None
     """
+    pass
+
+@action
+def send_request(request: str, target_agent: str) -> str:
+    """
+    Send a text request to the fellow agents.
     
-    def __init__(self, agent_name: str, action_server: ActionServer, env:Env) -> None:
-        self.agent_name = agent_name
-        self.action_server = action_server
-        self.env = env
-        
-    def get_agents(self) -> List[str]:
-        """
-        Get the list of agents in the environment.
-        """
-        raise NotImplementedError
+    Args:
+        request: The text request to send.
+        target_agent: The agent to send the request to.
     
-    def send_request(self, request: str, target_agent: str) -> str:
-        """
-        Send a text request to the fellow agents.
-        """
-        # TODO: This should not be part of BaseActions, but a part of the LLM agent.
-        raise NotImplementedError
+    """
+    pass
+
+@action
+def nav_to_obj(target_obj: str):
+    """
+    Navigate to an object.
+    
+    Args:
+        target_obj: The object to navigate to.
+    """
+    pass
+
+@action
+def nav_to_goal(goal: str):
+    """
+    Navigate to a goal.
+    
+    Args:
+        goal: The goal to navigate to.
+    """
+    pass
