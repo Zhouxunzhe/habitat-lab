edeb423235e3acf112c483b5336a72f97e14c25b
SgtVincent <sgt79911030@gmail.com>
2024-05-13 21:28:40 +0800

    add minimal actions for crab agents

---
+++ habitat-mas/habitat_mas/agents/actions/arm_actions.py
@@ -0,0 +1,29 @@
+import os
+from typing import List, Dict, Tuple, Union
+import numpy as np
+from habitat import Env
+
+from crab import action
+
+
+@action
+def pick(target_obj: str):
+    """
+    Pick an object.
+    
+    Args:
+        target_obj: The object to pick.
+    """
+    pass
+
+
+@action
+def place(target_obj: str, target_location: str):
+    """
+    Place an object.
+    
+    Args:
+        target_obj: The object to place.
+        target_location: The location to place the object.
+    """
+    pass
