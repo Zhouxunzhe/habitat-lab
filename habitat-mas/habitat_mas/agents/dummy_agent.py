4569710c18ffd838aa1ab7479e0e1591e228ad33
Zhouxunzhe <1170543554@qq.com>
2024-07-02 00:06:11 +0800

    [Development] update robot config for perception/manipulation

--- habitat-mas/habitat_mas/agents/dummy_agent.py
+++ habitat-mas/habitat_mas/agents/dummy_agent.py
@@ -1,6 +1,8 @@
-from typing import List, Tuple, Dict, Union, Callable
+from typing import List, Tuple, Dict, Union, Callable, Optional
 import openai
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import random
+
 
 # from habitat_mas.agents.llm_agent_base import LLMAgentBase
 
@@ -9,19 +11,39 @@ class DummyAgent:
     def __init__(self, **kwargs):
         # TODO check if agent_index is needed in llm agent
         self.agent_index = kwargs.get("agent_index", 0)
-        
-        
-    def chat(self, message: str) -> str:
+
+    def chat(self, message: str) -> Optional[dict]:
         """
         Mimic a chatbot always sending the oraclenav action.
         """
-        
-        return {
-            "name": "nav_to_obj",
-            "arguments": {
-                # always navigate to target object with pddl idx 1
-                "target_obj": "any_targets|1"
-            }
-        }
-         
 
+        return random.choice([
+            # {
+            #     "name": "nav_to_obj",
+            #     "arguments": {
+            #         "target_obj": "any_targets|0"
+            #     }
+            # },
+            {
+                "name": "nav_to_goal",
+                "arguments": {
+                    "target_obj": "any_targets|0"
+                }
+            },
+            # {
+            #     "name": "pick",
+            #     "arguments": {
+            #         "any_targets|0": "any_targets|0",
+            #         "robot_0": "robot_0",
+            #     }
+            # },
+            # {
+            #     "name": "place",
+            #     "arguments": {
+            #         "any_targets|0": "any_targets|0",
+            #         "TARGET_any_targets|0": "TARGET_any_targets|0",
+            #         "robot_0": "robot_0",
+            #     }
+            # },
+            ]
+        )
