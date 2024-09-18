edb07579d8c97091f3645e1c63e05e620368b068
HHYHRHY <15889568077@163.com>
2024-09-18 17:40:11 +0800

    alittlefix

--- dp_config.py
+++ dp_config.py
@@ -1,8 +1,8 @@
-sum_episode = 5000
+sum_episode = 4
 epnum_per_gz = 4
 gz_start = 0
 skip_len = 40
 base_directory = './video_dir/'
-process_num = 8
+process_num = 1
 gpu_num = 2
 sample_scene = ["102344115","102344193"]
